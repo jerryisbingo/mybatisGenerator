9b755195136d1186e5d226d7aeeefe5e1de7bd54
chenbin <3434677021@qq.com>
2017-12-07 13:39:43 +0800

    mybatis 代码生成

---
+++ src/main/java/com/cb/pojo/User.java
@@ -0,0 +1,53 @@
+package com.cb.pojo;
+
+public class User {
+    private Long id;
+
+    private String code;
+
+    private String name;
+
+    private Integer age;
+
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+}
